990f2ef314102e3ed5f638db428021ca74811c79
ArashEmp <ArashEmp@users.noreply.github.com>
2016-04-01 09:33:49 -0700

    Update redis.lua

--- libs/redis.lua
+++ libs/redis.lua
@@ -1,6 +1,6 @@
 local Redis = require 'redis'
 local FakeRedis = require 'fakeredis'
-
+ 
 local params = {
   host = os.getenv('REDIS_HOST') or '127.0.0.1',
   port = tonumber(os.getenv('REDIS_PORT') or 6379)
